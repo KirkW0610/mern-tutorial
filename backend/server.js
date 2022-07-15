63f703a99ae9005ea83c0ede87befe2175ebd6ce
KirkW0610 <kwhitaker0610@gmail.com>
2022-07-15 16:13:16 -0400

    Added Controller and AsyncHandler

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const express = require('express');
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port  = 5000;
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
